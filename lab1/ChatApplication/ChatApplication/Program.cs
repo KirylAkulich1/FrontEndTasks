2ee33014ebd2f43af9e4f6b51a99c48d39d2e15c
kirillakulich <kirillakulich0@gmail.com>
2021-02-24 21:13:51 +0300

    program.cs fixed

--- lab1/ChatApplication/ChatApplication/Program.cs
+++ lab1/ChatApplication/ChatApplication/Program.cs
@@ -8,37 +8,6 @@ namespace ChatApplication
 {
     class Program
     {
-        /// <summary>
-        /// args[0]=remote address
-        /// args[1]= remote port
-        /// args[2]=local port
-        /// </summary>
-        /// <param name="args"></param>
-        enum  ConnectionState
-        {
-            CONNECTED,
-            DISCONNECTED,
-            CLOSED
-        }
-        static async Task  DoFirstConnectAsync(string remoteAddress,int remotePort,string userName)
-        {
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
-            socket.Connect(localIP);
-            try
-            {
-                while (!socket.Connected)
-                {
-                    await socket.ConnectAsync(localIP);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                Console.WriteLine("");
-            }
-        }
-
         static void Main(string[] args)
         {
             ConfigureChat("127.0.0.1", 4444, 5555);
