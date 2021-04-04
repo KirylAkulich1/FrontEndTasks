71315565cad38a4b8c795ef6950268e6a4f56b1e
kirillakulich <kirillakulich0@gmail.com>
2021-04-04 20:30:47 +0300

    css added

--- fe_lab3/js/main.js
+++ fe_lab3/js/main.js
@@ -1,10 +1,12 @@
 function setTheme(themeName) {
+    console.log('Theme set');
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
 }
 
 // function to toggle between light and dark theme
 function toggleTheme() {
+    console.log('Toggle theme');
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-light');
     } else {
@@ -14,6 +16,7 @@ function toggleTheme() {
 
 // Immediately invoked function to set the theme on initial load
 (function () {
+    console.log('Theme called');
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-dark');
     } else {
